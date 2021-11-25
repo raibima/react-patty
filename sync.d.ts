f69b5377a2a1e26459859a990a5b00a66727ec92
Raibima Putra <rayp1100@gmail.com>
2021-11-25 11:26:50 +0700

    GitHub actions (#1)
    * Update declaration files
    
    * Add scripts + files fields
    
    * Add build workflow
    
    * Fix syntax
    
    * Fix syntax again
    
    * Try fix indentation
    
    * Try fix again
    
    * Do install in a separate step

--- sync.d.ts
+++ sync.d.ts
@@ -1,3 +1,3 @@
-import * as React from 'react';
-import { State } from './types';
-export declare function createState<S, A>(initialValue: S, reducer: React.Reducer<S, A>): State<S, A>;
+import { Reducer } from 'react';
+import type { State } from './types';
+export declare function createState<S, A>(initialValue: S, reducer: Reducer<S, A>): State<S, A>;

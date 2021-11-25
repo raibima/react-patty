38cee674eec2328281798ddff5755616b1fdc4c4
Raibima Putra <rayp1100@gmail.com>
2021-11-25 14:30:39 +0700

    DevX: print debug value

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,27 @@
-import {Dispatch, useContext} from 'react';
+import {Dispatch, useContext, useDebugValue} from 'react';
 import {State} from './types';
 
 export function useValue<S, A>(state: State<S, A>): S {
-  return useContext(state.__internal.valueContext);
+  const value = useContext(state.__internal.valueContext);
+  if (__DEV__) {
+    // Pretty print the value in devtools.
+    //
+    // if displayName property is set, use it as the label.
+    //    e.g., 'Counter: {value}'
+    //
+    // otherwise, just display the value without any label.
+    //    e.g., '{value}'
+    //
+    let debugValue = value + '';
+    let displayName = state.displayName;
+    if (typeof displayName === 'string') {
+      debugValue = `${displayName}: ${debugValue}`;
+    }
+    // this is permissible because __DEV__ is always static.
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useDebugValue(debugValue);
+  }
+  return value;
 }
 
 export function useDispatch<S, A>(state: State<S, A>): Dispatch<A> {

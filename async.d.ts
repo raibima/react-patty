d9de76d21a176721618553f55b6983bb713d7a3f
Raibima Putra <rayp1100@gmail.com>
2021-11-25 09:56:48 +0700

    Update declaration files

--- async.d.ts
+++ async.d.ts
@@ -1,11 +1,11 @@
-import * as React from 'react';
-import { State } from './types';
+import { Context, Reducer } from 'react';
+import type { State } from './types';
 interface AsyncState<S, A> extends State<S, A> {
     __internal: State<S, A>['__internal'] & {
-        loadStatusContext: React.Context<Status>;
+        loadStatusContext: Context<Status>;
     };
 }
-export declare function createAsyncState<S, A>(initialValue: S, resolver: () => Promise<S>, reducer: React.Reducer<S, A>): AsyncState<S, A>;
+export declare function createAsyncState<S, A>(initialValue: S, resolver: () => Promise<S>, reducer: Reducer<S, A>): AsyncState<S, A>;
 declare type Status = 'loading' | 'resolved';
 export declare function useLoadStatus<S, A>(state: AsyncState<S, A>): Status;
 export {};
